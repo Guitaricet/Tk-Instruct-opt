5b6260b133e5516264533506aa30720b7642c896
Nammu <namratashivagunde@gmail.com>
2023-01-05 12:18:07 -0500

    run3 corruptions on 30b

--- src/run_all_corruptions.py
+++ src/run_all_corruptions.py
@@ -38,52 +38,52 @@ os.system('python src/run-opt.py \
     --max_target_length 5 \
     --output_dir {}'.format(data_dir, task_dir, modelname, b1_corruption, max_num_instances_per_eval_task, b1_output_dir))
 
-#baseline2
-# print("####### Running Baseline2 ##############")
-# os.system('python src/run-opt.py \
-#     --data_dir {} \
-#     --task_dir {} \
-#     --modelname {} \
-#     --corruption {} \
-#     --overwrite_cache \
-#     --max_num_instances_per_task 2 \
-#     --max_num_instances_per_eval_task {} \
-#     --add_task_definition False \
-#     --num_pos_examples 4 \
-#     --num_neg_examples 0 \
-#     --add_explanation False \
-#     --max_source_length 1024 \
-#     --max_target_length 5 \
-#     --output_dir {}'.format(data_dir, task_dir, modelname, b2_corruption, max_num_instances_per_eval_task, b2_output_dir))
+# baseline2
+print("####### Running Baseline2 ##############")
+os.system('python src/run-opt.py \
+    --data_dir {} \
+    --task_dir {} \
+    --modelname {} \
+    --corruption {} \
+    --overwrite_cache \
+    --max_num_instances_per_task 2 \
+    --max_num_instances_per_eval_task {} \
+    --add_task_definition False \
+    --num_pos_examples 4 \
+    --num_neg_examples 0 \
+    --add_explanation False \
+    --max_source_length 1024 \
+    --max_target_length 5 \
+    --output_dir {}'.format(data_dir, task_dir, modelname, b2_corruption, max_num_instances_per_eval_task, b2_output_dir))
 
 
 
-# corruptions_list = ['instr-placement-before-ex']#, 
-#                     # 'instr-placement-after-ex',
-#                     # 'instr-randomwords',
-#                     # 'instr-frequentwords',
-#                     # 'label-random-labelspace',
-#                     # 'label-random-labelspace-half',
-#                     # 'label-randomwords',
-#                     # 'label-empty',
-#                     # 'input-empty',
-#                     # 'input-oodrandom']
+corruptions_list = ['instr-placement-before-ex']#, 
+                    # 'instr-placement-after-ex',
+                    # 'instr-randomwords',
+                    # 'instr-frequentwords',
+                    # 'label-random-labelspace',
+                    # 'label-random-labelspace-half',
+                    # 'label-randomwords',
+                    # 'label-empty',
+                    # 'input-empty',
+                    # 'input-oodrandom']
 
-# for corrup in corruptions_list:
-#     output_dir="output/default/{}/{}".format(modelfolder,corrup)    
-#     print("####### Running {} ##############".format(corruptions_list))
-#     os.system('python src/run-opt.py \
-#         --data_dir {} \
-#         --task_dir {} \
-#         --modelname {} \
-#         --corruption {} \
-#         --overwrite_cache \
-#         --max_num_instances_per_task 2 \
-#         --max_num_instances_per_eval_task {} \
-#         --add_task_definition True \
-#         --num_pos_examples 4 \
-#         --num_neg_examples 0 \
-#         --add_explanation False \
-#         --max_source_length 1024 \
-#         --max_target_length 5\
-#         --output_dir {}'.format(data_dir, task_dir, modelname, corrup, max_num_instances_per_eval_task, output_dir))
+for corrup in corruptions_list:
+    output_dir="output/default/{}/{}".format(modelfolder,corrup)    
+    print("####### Running {} ##############".format(corruptions_list))
+    os.system('python src/run-opt.py \
+        --data_dir {} \
+        --task_dir {} \
+        --modelname {} \
+        --corruption {} \
+        --overwrite_cache \
+        --max_num_instances_per_task 2 \
+        --max_num_instances_per_eval_task {} \
+        --add_task_definition True \
+        --num_pos_examples 4 \
+        --num_neg_examples 0 \
+        --add_explanation False \
+        --max_source_length 1024 \
+        --max_target_length 5\
+        --output_dir {}'.format(data_dir, task_dir, modelname, corrup, max_num_instances_per_eval_task, output_dir))
